9c13c14c7ddaa01f03d52c02d015998d3dd7557e
Aay78 <71471836+Aay78@users.noreply.github.com>
2021-01-22 18:16:31 -0700

    Add files via upload

---
+++ List/index.js
@@ -0,0 +1,14 @@
+
+        function showName(){
+            document.getElementById('display').innerHTML =
+            document.getElementById("name_input").value;
+            document.getElementById("name_input").value;
+        }
+
+var x = prompt("What is your age?")
+if (x  < 21){
+    alert("You can not acess our alcoholic beverages")
+}else{
+    alert("You may access out alcoholic beverages")
+    }
+
